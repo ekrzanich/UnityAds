e540f578db34be5a26c695fc05760d7d4d716412
Elora Krzanich <elorakrzanich@RyAdmins-MacBook-Pro-3.local>
2016-09-02 08:56:49 -0700

    Completed Unity project with Ads

---
+++ Unity_EloraKrzanich/Assets/Scripts/Scale.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Advertisements;
+
+public class Scale : MonoBehaviour {
+
+	public GameObject[] notes = new GameObject[8];
+	private List<GameObject> displayNotes = new List<GameObject> ();
+	private float currentXPos = 0f;
+	// Use this for initialization
+	void Start () {
+		ResetXPos ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private void ResetXPos()
+	{
+		currentXPos = 0;
+	}
+
+	public void ClearDisplay()
+	{
+		for (int i = 0; i < displayNotes.Count; i++) {
+			GameObject.Destroy (displayNotes [i]);
+		}
+
+		displayNotes.Clear ();
+		ResetXPos ();
+
+		if (Advertisement.IsReady()) {
+			Advertisement.Show ();
+		}
+	}
+
+	public void AddNote( int note )
+	{
+		if (displayNotes.Count < 8) {
+			GameObject newNote = (GameObject)GameObject.Instantiate (notes [note]);
+			newNote.transform.localPosition = new Vector3 (currentXPos, 0.25f * note);
+			newNote.GetComponent<RectTransform> ().SetParent (this.GetComponent<RectTransform> ().transform);
+			displayNotes.Add (newNote);
+			currentXPos += notes [note].GetComponent<SpriteRenderer> ().bounds.size.x;
+		}
+	}
+}
---
+++ Unity_EloraKrzanich/Assets/Scripts/Song.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Song : MonoBehaviour {
+
+	public AudioClip[] possibleNotes = new AudioClip[8];
+	private List<AudioClip> fullSong = new List<AudioClip>();
+	private bool isPlaying = false;
+	private int currentNote = 0;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = this.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isPlaying) {
+			if (!audioSource.isPlaying && fullSong.Count > currentNote) {
+				audioSource.clip = fullSong[currentNote];
+				audioSource.Play ();
+				currentNote++;
+			}
+		}
+	}
+
+	public void AddNote( int note )
+	{
+		if (fullSong.Count < 8) {
+			fullSong.Add (possibleNotes [note]);
+		}
+	}
+
+	public void ClearSong()
+	{
+		fullSong.Clear ();
+
+	}
+
+	public void PlaySong()
+	{
+		isPlaying = true;
+		currentNote = 0;
+	}
+
+	public void StopSong()
+	{
+		isPlaying = false;
+	}
+}
